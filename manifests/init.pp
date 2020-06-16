e471cd8ec5a9ad0628572094a1a318bf6f6887c0
tr <tr@netlab.li>
2020-06-16 21:37:16 +0200

    Set the crypto-policy level.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 # === Parameters
 #
-# [*policy_level*]
+# [*policy*]
 #   (Enum['default', 'legacy', 'future', 'fips']) The individual policy levels
 #   are included in the crypto-policies(7) package. In the future, there will
 #   be also a mechanism for easy creation and deployment of policies defined by
@@ -15,4 +15,12 @@
 class crypto_policy (
   $policy = 'default',
 ) {
+
+  if versioncmp($facts['os']['release']['major'],'8') >= 0 {
+    $policy_level = $policy.upcase()
+    exec {'update-crypto-policies':
+      command => "update-crypto-policies --set ${policy_level}",
+      unless  => "update-crypto-policies --show | grep -q ^${policy_level}$",
+    }
+  }
 }
---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,38 @@
+require File.expand_path(File.join(File.dirname(__FILE__),'../spec_helper'))
+describe 'crypto_policy', :type => 'class' do
+
+  # pre_condition definition
+  let(:pre_condition) do
+    "
+    Exec{
+      path => '/usr/sbin:/usr/bin:/sbin:/bin'
+    }
+    "
+  end
+
+  describe 'CentOS 8 standard' do
+    let(:facts) {
+      Octofacts.from_file('centos8.example.com.yaml')
+    }
+
+    it { should compile.with_all_deps }
+
+    it { should contain_exec('update-crypto-policies').with(
+      :command => 'update-crypto-policies --set DEFAULT',
+      :unless  => 'update-crypto-policies --show | grep -q ^DEFAULT$',
+    )}
+
+    context 'with policy level future' do
+      let(:params) {{
+        :policy => 'future',
+      }}
+
+      it { should compile.with_all_deps }
+
+      it { should contain_exec('update-crypto-policies').with(
+        :command => 'update-crypto-policies --set FUTURE',
+        :unless  => 'update-crypto-policies --show | grep -q ^FUTURE$',
+      )}
+    end
+  end
+end
